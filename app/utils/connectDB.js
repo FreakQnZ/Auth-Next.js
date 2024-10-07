cbfa3bf0683d8f7e068b7c5dbec3589f0ec610de
FreakQnZ <anuraag.srivatsa123@gmail.com>
2024-10-07 20:03:22 +0530

    changes to pool

--- app/utils/connectDB.js
+++ app/utils/connectDB.js
@@ -3,8 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL,
-    connectionString: "postgres://default:92kYcTRFZVpS@ep-holy-firefly-a1ozh2x5-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+  connectionString: process.env.POSTGRES_URL,
 })
 
 export default pool;
